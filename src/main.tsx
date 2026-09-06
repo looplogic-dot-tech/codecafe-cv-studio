a3711a47686f038a6e8f7bf705bb090785e2be48
Jaime Sanchez <34150357+looplogic-dot-tech@users.noreply.github.com>
2026-09-06 12:07:11 -0700

    Initialize A4 print preview

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import "./print-preview.css";
+import { installPrintPreview } from "./printPreview";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 );
+
+installPrintPreview();
