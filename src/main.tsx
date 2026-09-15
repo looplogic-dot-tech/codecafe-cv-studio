f2d695c012980afa477fef7af3c20ecca8c68f2b
Jaime Sanchez <34150357+looplogic-dot-tech@users.noreply.github.com>
2026-09-14 21:42:21 -0700

    Restore inherited page-break baseline in 1.6.4

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import "./professional-library.css";
 import "./library-composer.css";
 import "./live-preview-pages.css";
 import { prepareLinkedWorkspaceFromStorage } from "./libraryLinking";
-import { installPrintEditorV3 } from "./printEditorV3";
+import { installPrintPreview } from "./printPreview";
 import { installLivePreviewPages } from "./livePreviewPages";
 import { installSyncNotice } from "./syncNotice";
 import { installGoogleDriveAutoLoadV153 } from "./google-drive-autoload-v153";
@@ -49,7 +49,7 @@ createRoot(document.getElementById("root")!).render(
   </StrictMode>,
 );
 
-installPrintEditorV3();
+installPrintPreview();
 installLivePreviewPages();
 installSyncNotice();
 installGoogleDriveAutoLoadV153();
